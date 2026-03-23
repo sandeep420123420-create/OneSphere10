91fa54f1f5339765d8525d0681a033614f2a4e7b
sandeep420123420-create <sandeep420123420@gmail.com>
2026-03-23 13:13:41 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -65,12 +65,6 @@ socket.on("room_full", msg => {
 // =====================
 // MESSAGE HISTORY
 // =====================
-socket.on("message_history", messages => {
-  messagesDiv.innerHTML = "";
-
-  messages.forEach(msg => {
-    addMessage(`${msg.username}: ${msg.text}`, msg.created_at);
-  });
 
   joinContainer.classList.add("hidden");
   chatContainer.classList.remove("hidden");
